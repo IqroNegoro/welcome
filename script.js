f4ee43348cf184cd25e8b0c2412dcafaa8325715
IqroNegoro <iqronegoro0@gmail.com>
2021-12-31 09:06:37 +0700

    SELESAI JUGA LO DOMgit add .!

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const nav = document.getElementsByClassName("nav")[0];
 const info = document.getElementsByClassName("info")[0]
 const search = document.getElementById("search");
+const container = document.getElementsByClassName("container")[0]
+let itemss = document.querySelectorAll(".item")
 
 nav.style.height = window.innerHeight + "px"
 window.addEventListener("resize", () => {
@@ -38,16 +40,30 @@ search.addEventListener("keypress", e => {
 // sidenav
 const open = document.getElementById("plus");
 const tambah = document.getElementsByClassName("tambah")[0]
-
 // open listener box
 open.addEventListener("click", () => {
     tambah.style.display = "block"
 })
 
-window.addEventListener("click", e => {
+function removing(target) {
+    let targeting = target
+    const select = Array.from(document.getElementsByClassName("bx-x"));
+    let index = select.indexOf(targeting);
+    let data = JSON.parse(localStorage.getItem("nav"));
+    data.splice(index, 1);
+    localStorage.setItem("nav", JSON.stringify(data))
+    targeting.parentElement.parentElement.style.display = "none"
+}
+
+container.addEventListener("click", e => {
     if (e.target === tambah) {
         tambah.style.display = "none";
     }
+
+    if (e.target.classList.contains("bx-x")) {
+        removing(e.target)
+    }
+
 })
 
 // create new bookmark
@@ -56,55 +72,81 @@ const nama = document.getElementById("nama")
 const url = document.getElementById("url")
 const image = document.getElementById("image")
 
+let callback = entries => {
+    entries.forEach(val => {
+        val.target.classList.toggle("active", val.isIntersecting)
+    })
+}
+
+let options = {
+    threshold: 1,
+    rootMargin: "150px",
+    root: nav
+}
+
+let observer = new IntersectionObserver(callback, options)
+
+function refresh() {
+    itemss = document.querySelectorAll(".item")
+    itemss.forEach(item => {
+        observer.observe(item)
+    });
+}
+
 if (!localStorage.getItem("nav")) {
     localStorage.clear();
     localStorage.setItem("nav", "[]")
 } else {
     let data = JSON.parse(localStorage.getItem("nav"))
     data.forEach(val => {
-        nav.innerHTML += `
+        nav.insertAdjacentHTML("beforeend", 
+        `
         <div class="item">
         <div class="remove">
         <i class="bx bx-x"></i>
         </div>
-        <a href="https://${val.alamat}" target="_blank">
+        <a href="https://${val.url}" target="_blank">
         <div class="logo">
-        <img src="${val.image}">
+        <img src="${val.img}">
         </div>
         <div class="text">
-        <span>${val.text}</span>
+        <span>${val.nama}</span>
         </div>
         </a>
         </div>`
+        )
     })
+    refresh()
 }
+
 
 
 function addItem(obj) {
     let data = JSON.parse(localStorage.getItem("nav"));
     data.push(obj)
-    localStorage.setItem("nav", JSON.stringify(data))
-    let create = document.createElement("div");
-    create.setAttribute("class", "item");
-    create.innerHTML = `
+    nav.insertAdjacentHTML("beforeend",
+    
+    `<div class="item">
     <div class="remove">
-        <i class="bx bx-x"></i>
+    <i class="bx bx-x"></i>
     </div>
-    <a href="https://${obj.alamat}" target="_blank">
+    <a href="https://${obj.url}" target="_blank">
     <div class="logo">
-        <img src="${obj.image}">
+    <img src="${obj.img}">
     </div>
     <div class="text">
-        <span>${obj.text}</span>
+    <span>${obj.nama}</span>
     </div>
-    </a>`
-    nav.appendChild(create)
+    </a>
+    </div>`
+    )
+    localStorage.setItem("nav", JSON.stringify(data))
 }
 
-function createNew(alamat, text, imageWeb) {
-  this.alamat = alamat;
-  this.text = text;
-  this.image = imageWeb;  
+function createNew(nama, url, img) {
+    this.nama = nama;
+    this.url = url;
+    this.img = img;
 }
 save.addEventListener("click", () => {
     let namaWeb = nama.value;
@@ -112,4 +154,12 @@ save.addEventListener("click", () => {
     let imgWeb = image.value;
     let create = new createNew(namaWeb, urlWeb, imgWeb)
     addItem(create)
+    tambah.style.display = "none";
+    nama.value = "";
+    url.value = "";
+    image.value = "";
+    refresh()
 })
+
+
+
